1effde512b22c9c659a79ed58514a46e4eb24516
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-10 20:53:59 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,77 @@
+# print (1)
+# print (2)
+# print (3)
+# print()
+# x = 1
+# print(x)
+# x=2
+# print(x)
+# x=3
+# print(x)
+# print()
+# x=1
+# print(x)
+# x=x+1
+# print(x)
+# x=x+1
+# print(x)
+# print()
+#x=1
+#while x <= 100:
+  #  print(x)
+  #  x = x + 1
+
+#ex1
+
+#x=1
+#while x<= 100:
+   # print (x)
+   # x=x+1
+
+#ex2
+
+#x=50
+#while x<= 100:
+#    print(x)
+ #   x = x+1
+
+#ex 3
+# from time import sleep
+# x= 10
+# while x>=0:
+#     print(x)
+#     sleep(1)
+#     x-=1
+# print("FOGOOO")
+
+#ex 4
+
+# numero= int(input("Digite um número ímpar:"))
+# x = 1
+# while x <= numero:
+#     print(x)
+#     x = x+2
+
+#ex 5
+# x = 3
+# while x <= 30:
+#     print(x)
+#     x = x + 3
+
+#ex 6
+# tabuada = int(input(" Tabuada do número:"))
+# x = 1
+# while x <= 10:
+#     print (f"{tabuada}x{x}=", tabuada * x)
+#     x = x + 1
+
+#ex 7
+# tabuada = int(input("Insira o número da tabuada: "))
+# x = int(input("Início da tabuada: "))
+# y = int(input("Fim da tabuada: "))
+# while x <= y:
+#     print(f"{tabuada} x {x} ="
+#     ,tabuada * x)
+#     x = x + 1
+
+
